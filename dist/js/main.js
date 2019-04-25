f955aee3cf595538feca0062e11ecb48a18828b6
littlefinger42 <lauriemail@protonmail.com>
2019-04-25 16:23:42 +0200

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,67 @@
+(function() {
+  const threshold = 0.8;
+  const animateClassName = "animateonscroll-animate";
+  let _elements = [];
+  let _intersectionObserver = null;
+
+  /**
+   * IntersectionObserver callback
+   * @param  {Array<IntersectionObserverEntry>} entries
+   * @param  {IntersectionObserver} observer
+   */
+  const onIntersection = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio >= threshold) {
+        entry.target.classList.add(animateClassName);
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  /**
+   * Enable animateonscroll by create new IntersectionObserver observing each element with the data attribute
+   */
+  const enable = () => {
+    _intersectionObserver = new IntersectionObserver(onIntersection, {
+      rootMargin: "0% 50%",
+      threshold: threshold
+    });
+
+    _elements = [].filter.call(
+      document.querySelectorAll("[data-animateonscroll]"),
+      _element => !_element.classList.contains(animateClassName)
+    );
+
+    _elements.forEach(_element => _intersectionObserver.observe(_element));
+  };
+
+  /**
+   * Init
+   * @param  {Object} settings
+   * @return {Object} public API
+   */
+  const init = () => {
+    if (!window.IntersectionObserver) {
+      throw Error(`
+		Your browser does not support IntersectionObserver!
+	  `);
+    }
+
+    enable();
+  };
+
+  window.animateOnScroll = init;
+})();
+
+(function(){
+	window.animateOnScroll();
+
+	window.addEventListener('scroll', function(e) {
+		var chapter = 0;
+		if (chapter = 0) {
+			
+			chapter = 1;
+			console.log(e);
+		}
+	});
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,68 @@
-var gulp = require('gulp');
-var gulp_sass = require('gulp-sass');
+var gulp = require("gulp");
+var gulp_sass = require("gulp-sass");
+var gulp_concat = require("gulp-concat");
+var gulp_rename = require("gulp-rename");
+var gulp_uglify = require("gulp-uglify");
 
-var browserSync = require('browser-sync').create();
+var browserSync = require("browser-sync").create();
 
 var config = {
-	paths: {
-		src: {
-			html: './src/*.html',
-			sass: './src/scss/**/*.scss'
-		},
-		dist: './dist'
+  paths: {
+    src: {
+      html: "./src/*.html",
+      js: "./src/js/*.js",
+      sass: "./src/scss/**/*.scss"
+    },
+    dist: {
+		html: "./dist",
+		js: "./dist/js",
+		css: "./dist/css",
 	}
-}
+  }
+};
 
-gulp.task('sass', () => {
-	return gulp.src(config.paths.src.sass)
-		.pipe(gulp_sass())
-		.pipe(gulp.dest(config.paths.dist + '/css/'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
-})
+gulp.task("sass", () => {
+  return gulp
+    .src(config.paths.src.sass)
+    .pipe(gulp_sass())
+    .pipe(gulp.dest(config.paths.dist.css))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
 
-gulp.task('html', () => {
-	return gulp.src(config.paths.src.html)
-		.pipe(gulp.dest(config.paths.dist))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
-})
+gulp.task("js", () => {
+  return gulp
+    .src(config.paths.src.js)
+    .pipe(gulp_concat("main.js"))
+    .pipe(gulp.dest(config.paths.dist.js));
+});
 
-gulp.task('browserSync', () => {
-	browserSync.init({
-		server: {
-			baseDir: config.paths.dist
-		}
-	})
-})
+gulp.task("html", () => {
+  return gulp
+    .src(config.paths.src.html)
+    .pipe(gulp.dest(config.paths.dist.html))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
 
-gulp.task('watch', () => {
-	gulp.series('browserSync');
-	gulp.watch(config.paths.src.sass, gulp.series('sass'));
-	gulp.watch(config.paths.src.html, gulp.series('html'));
-	// Other watchers
-})
+gulp.task("browserSync", () => {
+  browserSync.init({
+    server: {
+      baseDir: config.paths.dist
+    }
+  });
+});
+
+gulp.task("watch", () => {
+  gulp.series("browserSync");
+  gulp.watch(config.paths.src.sass, gulp.series("sass"));
+  gulp.watch(config.paths.src.js, gulp.series("js"));
+  gulp.watch(config.paths.src.html, gulp.series("html"));
+  // Other watchers
+});
