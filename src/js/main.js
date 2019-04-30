008897754acdde351628e8e53dedbd4afe713ad9
littlefinger42 <lauriemail@protonmail.com>
2019-04-30 12:45:16 +0200

    Improved layout of bio items

--- src/js/main.js
+++ src/js/main.js
@@ -10,38 +10,45 @@
     portfolioContainer.appendChild(article);
 
     if (portfolioItems[i].background.type === "video") {
-      var videoElement = document.createElement("video");
-      videoElement.setAttribute("autoplay", true);
-      videoElement.setAttribute("loop", true);
-      videoElement.setAttribute("async", true);
-      videoElement.muted = true;
-
-      var sourceElementMp4 = document.createElement("source");
-      sourceElementMp4.setAttribute("type", "video/mp4");
-      sourceElementMp4.src =
-        "videos/port/" + portfolioItems[i].background.src + ".mp4";
-      var sourceElementWebm = document.createElement("source");
-      sourceElementWebm.setAttribute("type", "video/webm");
-      sourceElementWebm.src =
-        "videos/port/" + portfolioItems[i].background.src + ".webm";
-
-      videoElement.appendChild(sourceElementMp4);
-      videoElement.appendChild(sourceElementWebm);
-	  article.appendChild(videoElement);
-	  
+      article.appendChild(createVideoElement(portfolioItems[i].background.src));
     } else if (portfolioItems[i].background.type === "img") {
+      article.appendChild(createImgElement(portfolioItems[i].background.src, portfolioItems[i].title + " portfolio image"));
+    }
 
-      var imgElement = document.createElement("img");
-      imgElement.src = "img/port/" + portfolioItems[i].background.src + ".jpg";
-      imgElement.alt = portfolioItems[i].title + " portfolio image";
-      article.appendChild(imgElement);
-	}
-	
     var buttonLink = document.createElement("a");
     buttonLink.textContent = "View " + portfolioItems[i].title;
     buttonLink.href = "/portfolio.html?id=" + portfolioItems[i].background.src;
     buttonLink.classList.add("btn-view", "p-small");
     buttonLink.classList.add("btn-view", "p-small");
     article.appendChild(buttonLink);
+  }
+
+  function createVideoElement(src) {
+    var videoElement = document.createElement("video");
+    videoElement.setAttribute("autoplay", true);
+    videoElement.setAttribute("loop", true);
+    videoElement.setAttribute("async", true);
+    videoElement.muted = true;
+
+    var sourceElementMp4 = document.createElement("source");
+    sourceElementMp4.setAttribute("type", "video/mp4");
+    sourceElementMp4.src =
+      "videos/port/" + src + ".mp4";
+    var sourceElementWebm = document.createElement("source");
+    sourceElementWebm.setAttribute("type", "video/webm");
+    sourceElementWebm.src =
+      "videos/port/" + portfolioItems[i].background.src + ".webm";
+
+    videoElement.appendChild(sourceElementMp4);
+    videoElement.appendChild(sourceElementWebm);
+
+    return videoElement;
+  }
+
+  function createImgElement(src, alt) {
+    var imgElement = document.createElement("img");
+    imgElement.src = "img/port/" + src + ".jpg";
+    imgElement.alt = alt;
+    return imgElement;
   }
 })();
